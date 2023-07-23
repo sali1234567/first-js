582abd893dd1d6a07d76fc76d01390d291b05021
sali1234567 <sali.chuguashvili@gmail.com>
2023-07-23 23:07:56 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+userName = 'Salome';
+lastName = 'Chuguashvili';
+userAge = 14;
+dwellingPlace = 'tbilisi'
+study = 'Goal-Oriented Academy'
+
+
+console.log('Hi, my name is ' + userName, lastName + ' I\'m ' + userAge, 'years old. and I live in ' + dwellingPlace + ", " +'I learned how to write this code in ' + study)
